9fbf611c2030876d8d118a07a5390cc17322820d
splitzerr <rishikgandash@gmail.com>
2026-06-15 03:39:36 +0530

    new: added jwt authentication alongside test suite (gitignored)

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,10 @@
 import { hash, verify } from "argon2";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken";
+
+
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+
 
 export async function hashPassword(password: string): Promise<string> {
     return hash(password);
@@ -6,4 +12,29 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function checkPasswordHash(password: string, hash: string): Promise<Boolean> {
     return verify(hash, password)
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    const userPayLoad: payload = {
+         "iss": "chirpy",
+         "sub": userID,
+         "iat": Math.floor(Date.now() / 1000),
+         "exp": Math.floor(Date.now() / 1000) + expiresIn,
+    };
+
+    return jwt.sign(userPayLoad, secret);
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    try {
+        const userPayLoad = jwt.verify(tokenString, secret);
+        const userId = typeof(userPayLoad) === 'string' ? userPayLoad : userPayLoad["sub"];
+        if(typeof(userId) === 'undefined') {
+            throw new Error("undefined payload error");
+        }
+        return userId;
+    }
+    catch (err) {
+        throw new Error(`auth.ts/validateJwt: ${err}, authentication failed`);
+    }
 }
