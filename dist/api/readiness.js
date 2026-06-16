295ba13f51a8eb9ac02f36284e1af3c3c4cb4263
splitzerr <rishikgandash@gmail.com>
2026-06-17 02:40:10 +0530

    chore: stop tracking generated files and tests

--- dist/api/readiness.js
+++
@@ -1,5 +0,0 @@
-export async function handlerReadiness(_, res) {
-    res.set('Content-Type', 'text/plain; charset=utf-8');
-    res.status(200);
-    res.send('OK');
-}
--- dist/healthz/handler.js
+++
@@ -1,5 +0,0 @@
-export async function handlerReadiness(req, res) {
-    res.set('Content-Type', 'text/plain');
-    res.status(200);
-    res.send('200 OK');
-}
